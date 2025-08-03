3f33130c925819430f920ba637f312187579f43c
Divya B Gowda <117468949+DivyaBGowda484@users.noreply.github.com>
2025-08-03 18:34:39 +0530

    Switch to dj-database-url for production database

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-import os
 import dj_database_url
+import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # for local dev
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
